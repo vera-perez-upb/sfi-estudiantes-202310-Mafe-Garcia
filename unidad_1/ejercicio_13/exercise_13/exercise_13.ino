6ac5752e48adfe7dbd53e45df4b579268956365a
Mafe-Garcia <88628459+Mafe-Garcia@users.noreply.github.com>
2024-02-28 09:20:04 -0500

    exercise 13

---
+++ unidad_1/ejercicio_13/exercise_13/exercise_13.ino
@@ -0,0 +1,61 @@
+static void changeVar(uint32_t *pdata)
+{
+    *pdata = 10;
+}
+
+static void printVar(uint32_t value)
+{
+    Serial.print("var content: ");
+    Serial.print(value);
+    Serial.print('\n');
+}
+
+void task1()
+{
+    enum class Task1States    
+    {
+        INIT,
+        WAIT_DATA
+    };
+    static Task1States task1State = Task1States::INIT;
+
+    switch (task1State)
+    {
+    case Task1States::INIT:
+    {
+        Serial.begin(115200);
+        task1State = Task1States::WAIT_DATA;
+        break;
+    }
+
+    case Task1States::WAIT_DATA:
+    {
+        // evento 1:        // Ha llegado al menos un dato por el puerto serial?        
+        if (Serial.available() > 0)
+        {
+            Serial.read();
+            uint32_t var = 0;
+            uint32_t *pvar = &var;
+            printVar(*pvar);
+            changeVar(pvar);
+            printVar(var);
+        }
+        break;
+    }
+
+    default:
+    {
+        break;
+    }
+    }
+}
+
+void setup()
+{
+    task1();
+}
+
+void loop()
+{
+    task1();
+}
