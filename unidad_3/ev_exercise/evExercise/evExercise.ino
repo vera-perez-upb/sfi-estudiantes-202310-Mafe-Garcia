78b062355cfa16fbf0fa2daeef2a935b99be0633
B09S112est <B09S112est@MEDAULAS.UPB>
2024-04-26 09:37:56 -0500

    evaluation

---
+++ unidad_3/ev_exercise/evExercise/evExercise.ino
@@ -0,0 +1,55 @@
+void task()
+{
+    enum class TaskStates
+    {
+        INIT,
+        WAIT_INIT,
+        SEND_EVENT
+    };
+    static TaskStates taskState = TaskStates::INIT;
+    float num = 3.8;  // 40 73 33 33
+    static uint8_t arr[4] = {0};
+
+    switch (taskState)
+    {
+        case TaskStates::INIT:
+        {
+            Serial.begin(115200);
+            taskState = TaskStates::WAIT_INIT;
+            break;
+        }
+        case TaskStates::WAIT_INIT:
+        {
+            if (Serial.available() > 0)
+            {
+                if (Serial.read() == '1')
+                {
+                    taskState = TaskStates::SEND_EVENT;
+                }
+            }
+            break;
+        }
+        case TaskStates::SEND_EVENT:
+        {
+          memcpy(arr,(uint8_t *)&num,4);
+          for(int8_t i = 3; i >= 0; i--){
+                        Serial.write(arr[i]);
+          }          
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+}
+
+void setup()
+{
+    task();
+}
+
+void loop()
+{
+    task();
+}
--- unidad_3/unit3Project/Assets/scripts/Controller.cs
+++ unidad_3/unit3Project/Assets/scripts/Controller.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO.Ports;
+using TMPro;
+
+enum TaskState
+{
+    INIT,
+    WAIT_START,
+    WAIT_EVENTS
+}
 
 public class Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private static TaskState taskState = TaskState.INIT;
+    private SerialPort _serialPort;
+    private byte[] buffer;
+    public TextMeshProUGUI myText;
+    private int counter = 0;
+    private bool buttonIsPressed = false;
+
     void Start()
     {
-        
+        _serialPort = new SerialPort();
+        _serialPort.PortName = "COM3";
+        _serialPort.BaudRate = 115200;
+        _serialPort.DtrEnable = true;
+        _serialPort.Open();
+        Debug.Log("Open Serial Port");
+        buffer = new byte[128];
     }
 
-    // Update is called once per frame
+    public void OnButtonPress()
+    {
+        buttonIsPressed = true;
+    }
+        
     void Update()
     {
-        
+        Debug.Log(counter.ToString());
+        counter++;
+
+        switch (taskState)
+        {
+            case TaskState.INIT:
+                taskState = TaskState.WAIT_START;
+                Debug.Log("WAIT START");
+                break;
+            case TaskState.WAIT_START:
+                if (buttonIsPressed == true)
+                {
+                    byte[] data = { 0x31 };// start
+                    _serialPort.Write(data, 0, 1);
+                    Debug.Log("WAIT EVENTS");
+                    taskState = TaskState.WAIT_EVENTS;
+                }
+                break;
+            case TaskState.WAIT_EVENTS:                
+                if (_serialPort.BytesToRead > 0)
+                {
+                    int numData = _serialPort.Read(buffer, 0, 128);
+                    myText.text = (System.Text.Encoding.ASCII.GetString(buffer));
+                }
+                break;
+            default:
+                Debug.Log("State Error");
+                break;
+        }
     }
 }
